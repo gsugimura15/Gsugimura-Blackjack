2ff7d7e3ea3ffcda78022f0315714ada8ab44bc9
gsugimura15 <gsugimura15@punahou.edu>
2015-03-18 12:40:11 -1000

    Switch and Cards
    Researched more about “switch” and decided to use it as part of my code
    (45 Minutes)
    
    Finished “first draft” of Cards class (20 Minutes)
    
    Unsure about the default part of switch, will have to continue learning
    about that.

--- Cards.java
+++ Cards.java
@@ -31,13 +31,30 @@ public class Card {
              return "Hearts";
            case Spades:    
              return "Spades";
-           default:       
-             return "";
+           //default:       
+             //return "";
         }
 
-    public String toString() {
-        //return (get value as a string) + " of " + (Suit as a string);
+   public String getStringValue() {
+        switch ( value ) {
+           case 1:   return "Ace";
+           case 2:   return "2";
+           case 3:   return "3";
+           case 4:   return "4";
+           case 5:   return "5";
+           case 6:   return "6";
+           case 7:   return "7";
+           case 8:   return "8";
+           case 9:   return "9";
+           case 10:  return "10";
+           case 11:  return "Jack";
+           case 12:  return "Queen";
+           case 13:  return "King";
+          // default
+        }
     }
-
+    
+    public String toString() {
+        //return getStringValue() + " of " + getStringSuit();
 
 } 
