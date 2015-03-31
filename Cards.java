e414dfc148731a6ba4fb20a94e624278ba36ca93
gsugimura15 <gsugimura15@punahou.edu>
2015-03-31 12:54:24 -1000

    Work log #...7?
    I did some more work on the deck and card classes as well as the
    blackjack hand class. I also added some more classes but they’re empty
    right now. I just tried to add the ones that I think I might need in
    the future.

--- Cards.java
+++ Cards.java
@@ -1,16 +1,22 @@
 public class Card {
 
-    public static int Clubs = 0, Diamonds = 1, Hearts = 2, Spades = 3;       
+    public static int Clubs = 0;
+    public static int Diamonds = 1;
+    public static int Hearts = 2;
+    public static int Spades = 3;       
                             
-    public static int Ace = 1, Jack = 11, Queen = 12, King = 13;       
+    public static int Ace = 1;
+    public static int Jack = 11; 
+    public static int Queen = 12; 
+    public static int King = 13;       
                             
     private int suit;   
                              
     private int value; 
                              
     public Card(int theValue, int theSuit) {
-        value = theValue;
-        suit = theSuit;
+        value = aValue;
+        suit = aSuit;
     }
         
     public int getSuit() {
@@ -31,30 +37,44 @@ public class Card {
              return "Hearts";
            case Spades:    
              return "Spades";
-           //default:       
-             //return "";
+           default:       
+             return "Invalid";
         }
 
    public String getStringValue() {
         switch ( value ) {
-           case 1:   return "Ace";
-           case 2:   return "2";
-           case 3:   return "3";
-           case 4:   return "4";
-           case 5:   return "5";
-           case 6:   return "6";
-           case 7:   return "7";
-           case 8:   return "8";
-           case 9:   return "9";
-           case 10:  return "10";
-           case 11:  return "Jack";
-           case 12:  return "Queen";
-           case 13:  return "King";
-          // default
+          case 1:   
+             return "Ace";
+           case 2:   
+             return "2";
+           case 3:   
+             return "3";
+           case 4:   
+             return "4";
+           case 5:   
+             return "5";
+           case 6:   
+             return "6";
+           case 7:   
+             return "7";
+           case 8:   
+             return "8";
+           case 9:   
+             return "9";
+           case 10:  
+             return "10";
+           case 11:  
+             return "Jack";
+           case 12:  
+             return "Queen";
+           case 13:  
+             return "King";
+           default:
+             return "Invalid";
         }
     }
     
     public String toString() {
-        //return getStringValue() + " of " + getStringSuit();
+        return getStringValue() + " of " + getStringSuit();
 
 } 
